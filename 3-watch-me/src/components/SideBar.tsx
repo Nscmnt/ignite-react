19b1e286fa7bd6f6f9f0c059f51b105f1ba54780
Jorge Nascimento <nascimento.dev@outlook.com>
2021-11-03 13:21:03 -0300

    watch-me

---
+++ 3-watch-me/src/components/SideBar.tsx
@@ -0,0 +1,39 @@
+import { Button } from "./Button";
+
+interface SIdeBarProps {
+  genres: Array<{
+    id: number;
+    title: string;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  }>;
+  handleClickButton: Function;
+  selectedGenreId: number;
+}
+
+import "../styles/sidebar.scss";
+
+export function SideBar({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}: SIdeBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
